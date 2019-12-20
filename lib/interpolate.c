469de1b2930eedd6c84b7e99323f2b31c2c16ab3
Douglas Rennehan <rennehan@uvic.ca>
2019-12-20 13:29:30 -0500

    Allow 2D as well as 3D maps. Specified by DIMS compilation options.

--- lib/interpolate.c
+++ lib/interpolate.c
@@ -6,6 +6,7 @@
 #define SIZE 1.0
 #define HALF 0.5
 #define MIN 0.0
+#define DIMS 2
 
 void out_of_memory(char var_name[], long long bytes)
 {
@@ -27,6 +28,7 @@ double adjusted_difference(double difference)
     return difference;
 }
 
+/* D. Rennehan: hinv3 could be hinv2 if DIMS == 2 */
 double cubic_kernel(double u, double hinv3)
 {
     double wk;
@@ -40,7 +42,11 @@ double cubic_kernel(double u, double hinv3)
         wk = 2.0 * t2 * t1;
     }
 
+#if (DIMS == 3)
     return (8.0 / M_PI) * wk * hinv3;
+#else
+    return (40.0 / (7.0 * M_PI)) * wk * hinv3;
+#endif
 }
 
 int real_idx(int idx, int N_cell)
@@ -73,7 +79,11 @@ void interpolate_to_grid(char tmp_file_name[],
     double U[N_cell];
 
     long long N_cell2 = N_cell * N_cell;
+#if (DIMS == 3)
     long long N_cell3 = N_cell2 * N_cell;
+#else
+    long long N_cell3 = N_cell2;
+#endif
 
     double delta = SIZE / N_cell;
 
@@ -109,24 +119,25 @@ void interpolate_to_grid(char tmp_file_name[],
     /* Important that we assume all coordinates are within [0,1] */
     for (i = 0; i < N_particles; i++)
     {
-        if (radii[i] < delta)
-        {
-            radii[i] = 2.0 * delta;
-        }
-
         /* These are the indices in the U vector */
         int max_x = (int)((pos_x[i] + radii[i]) / delta);
         int min_x = (int)((pos_x[i] - radii[i]) / delta);
 
         int max_y = (int)((pos_y[i] + radii[i]) / delta);
         int min_y = (int)((pos_y[i] - radii[i]) / delta);
-    
+   
+#if (DIMS == 3) 
         int max_z = (int)((pos_z[i] + radii[i]) / delta);
         int min_z = (int)((pos_z[i] - radii[i]) / delta);
+#endif
 
         int num_x_cells = max_x - min_x + 1;
         int num_y_cells = max_y - min_y + 1;
+#if (DIMS == 3)
         int num_z_cells = max_z - min_z + 1;
+#else
+        int num_z_cells = 1;
+#endif
 
         long total_cell_chunk = (long)(num_x_cells * num_y_cells * num_z_cells);
         long long chunk_memory_bytes = total_cell_chunk * sizeof(int);
@@ -143,11 +154,13 @@ void interpolate_to_grid(char tmp_file_name[],
             out_of_memory("deposit_y", chunk_memory_bytes);
         }
 
+#if (DIMS == 3)
         int *deposit_z = (int *)malloc(chunk_memory_bytes);
         if (deposit_z == NULL)
         {
             out_of_memory("deposit_z", chunk_memory_bytes);
         }
+#endif
 
         double *temp_weights = (double *)malloc(total_cell_chunk * sizeof(double));
         if (temp_weights == NULL)
@@ -159,8 +172,10 @@ void interpolate_to_grid(char tmp_file_name[],
         long long x_double_bytes = (long long)(num_x_cells * sizeof(double));
         long long y_int_bytes = (long long)(num_y_cells * sizeof(int));
         long long y_double_bytes = (long long)(num_y_cells * sizeof(double));
+#if (DIMS == 3)
         long long z_int_bytes = (long long)(num_z_cells * sizeof(int));
         long long z_double_bytes = (long long)(num_z_cells * sizeof(double));
+#endif
 
         int *x_indices = (int *)malloc(x_int_bytes);
         if (x_indices == NULL)
@@ -174,11 +189,13 @@ void interpolate_to_grid(char tmp_file_name[],
             out_of_memory("y_indices", y_int_bytes);
         }
 
+#if (DIMS == 3)
         int *z_indices = (int *)malloc(z_int_bytes);
         if (z_indices == NULL)
         {
             out_of_memory("z_indices", z_int_bytes);
         }
+#endif
 
         double *x_diffs = (double *)malloc(x_double_bytes);
         if (x_diffs == NULL)
@@ -192,11 +209,13 @@ void interpolate_to_grid(char tmp_file_name[],
             out_of_memory("y_diffs", y_double_bytes);
         }
 
+#if (DIMS == 3)
         double *z_diffs = (double *)malloc(z_double_bytes);
         if (z_diffs == NULL)
         {
             out_of_memory("z_diffs", z_double_bytes);
         }
+#endif
 
         double distance = 0, sum_weights = 0;
         int running_idx = 0;
@@ -217,6 +236,7 @@ void interpolate_to_grid(char tmp_file_name[],
             running_idx++;
         }
 
+#if (DIMS == 3)
         running_idx = 0;
 
         for (j = min_z; j <= max_z; j++)
@@ -225,6 +245,7 @@ void interpolate_to_grid(char tmp_file_name[],
             z_diffs[running_idx] = adjusted_difference(pos_z[i] - U[z_indices[running_idx]]);
             running_idx++;
         }
+#endif
 
         running_idx = 0;
 
@@ -246,7 +267,9 @@ void interpolate_to_grid(char tmp_file_name[],
             temp_weights[0] = 1.0;
             deposit_x[0] = x_indices[0];
             deposit_y[0] = y_indices[0];
+#if (DIMS == 3)
             deposit_z[0] = z_indices[0];
+#endif
 
             sum_weights += 1.0;
             running_idx++;
@@ -261,23 +284,34 @@ void interpolate_to_grid(char tmp_file_name[],
         {
             for (k = 0; k < num_y_cells; k++)
             {
+#if (DIMS == 3)
                 for (l = 0; l < num_z_cells; l++)
                 {
                     distance = sqrt(x_diffs[j] * x_diffs[j] + y_diffs[k] * y_diffs[k] + z_diffs[l] * z_diffs[l]);
-
+#else
+                    distance = sqrt(x_diffs[j] * x_diffs[j] + y_diffs[k] * y_diffs[k]);
+#endif
                     temp_weights[running_idx] = 0;
                     deposit_x[running_idx] = x_indices[j];
                     deposit_y[running_idx] = y_indices[k];
+#if (DIMS == 3)
                     deposit_z[running_idx] = z_indices[l];
+#endif
 
                     if (distance < radii[i])
                     {
+#if (DIMS == 3)
                         temp_weights[running_idx] = cubic_kernel(distance / radii[i], 1.0 / (radii[i] * radii[i] * radii[i]));
+#else
+                        temp_weights[running_idx] = cubic_kernel(distance / radii[i], 1.0 / (radii[i] * radii[i]));
+#endif
                         sum_weights += temp_weights[running_idx];
                     }
 
                     running_idx++;
+#if (DIMS == 3)
                 }
+#endif
             }
         }
 
@@ -290,20 +324,30 @@ void interpolate_to_grid(char tmp_file_name[],
             wk = temp_weights[j] / sum_weights;
             true_weight = weights[i] * wk;
 
+#if (DIMS == 3)
             map_idx = N_cell2 * deposit_x[j] + N_cell * deposit_y[j] + deposit_z[j];
+#else
+            map_idx = N_cell * deposit_x[j] + deposit_y[j];
+#endif
    
             map[map_idx] += quantities[i] * true_weight;
             map_weights[map_idx] += true_weight;
         }
 
+#if (DIMS == 3)
         free(z_diffs);
+#endif
         free(y_diffs);
         free(x_diffs);
+#if (DIMS == 3)
         free(z_indices);
+#endif
         free(y_indices);
         free(x_indices);
         free(temp_weights);
+#if (DIMS == 3)
         free(deposit_z);
+#endif
         free(deposit_y);
         free(deposit_x); 
     }
